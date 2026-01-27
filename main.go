2ddb3cedbfb6c69124511353cadbcbd1073e0385
Edgar Lins <linss.edgar@gmail.com>
2026-01-27 19:51:31 -0300

    up server

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerURL(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Encurtador de URL Fisrt step!")
+}
+
 func main() {
-	println("Hello, World!")
+	http.HandleFunc("/", handlerURL)
+	println("Servidor rodando na porta http://localhost:8080")
+
+	http.ListenAndServe(":8080", nil)
 }
